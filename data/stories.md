429f21b0592d5d55749c670859d34a7522dc6578
sayeeayachit <54578191+sayeeayachit@users.noreply.github.com>
2019-08-27 23:20:52 +0530

    Add files via upload

---
+++ data/stories.md
@@ -0,0 +1,19 @@
+## story 01
+* greet
+   - utter_greet
+
+## story 02
+* goodbye
+   - utter_goodbye
+
+## story 03
+* inform
+  - utter_ask_location
+
+## story 04
+* inform
+   -utter_ask_dot
+
+
+
+
---
+++ nlu_model.py
@@ -0,0 +1,19 @@
+from rasa_nlu.converters import load_data
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Trainer
+from rasa_nlu.model import Metadata, Interpreter
+
+def train_nlu(data, config, model_dir):
+	training_data = load_data(data)
+	trainer = Trainer(RasaNLUConfig(config))
+	trainer.train(training_data)
+	model_directory = trainer.persist(model_dir, fixed_model_name = 'railwaynlu')
+	
+def run_nlu():
+	interpreter = Interpreter.load('./models/nlu/default/railwaynlu', RasaNLUConfig('config_spacy.json'))
+	print(interpreter.parse("where is 12345 train now"))
+	
+if __name__ == '__main__':
+	#train_nlu('./data/data.json', 'config_spacy.json', './models/nlu')
+	run_nlu()
+
